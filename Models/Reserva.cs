84740a917c46101ff347cf3bbcc38b6637015e92
Ewerton <eww999@hotmail.com>
2024-09-12 00:55:31 -0300

    first commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() 
+        {
+            Hospedes = new List<Pessoa>();
+        }
+
+        public Reserva(int diasReservados) : this()
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite != null && hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new InvalidOperationException("A capacidade da suíte é menor que o número de hóspedes.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+         
+            if (Suite == null)
+            {
+                throw new InvalidOperationException("A suíte não foi cadastrada.");
+            }
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m; 
+            }
+
+            return valor;
+        }
+    }
+
+    
+}
